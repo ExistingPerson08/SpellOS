068cd30c7f3b32051d8cec58677d1be756f9887a
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-05-01 11:08:31 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,15 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Create autostart script
+sudo mkdir -p /etc/skel/.config/autostart && sudo tee /etc/skel/.config/autostart/steam-game-install.desktop > /dev/null <<EOF
+[Desktop Entry]
+Version=1.0
+Name=Spacefin welcome
+Comment=Welcome to your Spacefin desktop!
+Exec=steam steam://install/39540
+Icon=steam
+Terminal=false
+Type=Application
+Categories=System;Utility;
+EOF
