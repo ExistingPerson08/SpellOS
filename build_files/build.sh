ea2189757b5009ac3abcdfdd7746b6f19afcd093
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-05-01 14:25:44 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,8 +6,8 @@ set -ouex pipefail
 mkdir -p /etc/skel/.config/autostart && tee /etc/skel/.config/autostart/steam-game-install.desktop > /dev/null <<EOF
 [Desktop Entry]
 Version=1.0
-Name=Spacefin welcome
-Comment=Welcome to your Spacefin desktop!
+Name=Install spellforce 1
+Comment=Install spellforce 1 using steam
 Exec=steam steam://install/39540
 Icon=steam
 Terminal=false
